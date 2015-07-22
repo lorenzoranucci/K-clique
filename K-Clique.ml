3fb989281c0d87dcb84ff462140b21f22b3a1b92
Lorenzo <loryzizu@gmail.com>
2015-07-22 18:38:58 +0200

    Relazione modificata, eliminate funzioni inutili

--- K-Clique.ml
+++ K-Clique.ml
@@ -1,47 +1,41 @@
-let grafo = [(1,[2;3;5]); (5,[2;3;4;1;7]) ;(7,[5;2;3;4]); (2,[4;1;5;3;7]); (3,[2;4;1;5;7]); (4,[2;3;5;7]) ];;
-
-let rec listaNodiGrafo grafo=
-  match grafo with 
+let rec queueForCliqueFromAdjGraph graph=
+  match graph with
       []->[]
-    | (x,adj)::rest-> x::(listaNodiGrafo rest);;
+    | (x,adj)::rest-> (x,[])::(queueForCliqueFromAdjGraph rest);;
 
-listaNodiGrafo grafo;;
 
-let rec grafoAdjToGrafoAdjCricca grafo=
-  match grafo with
-      []->[]
-    | (x,adj)::rest-> (x,[])::(grafoAdjToGrafoAdjCricca rest);;
-
-grafoAdjToGrafoAdjCricca grafo;;
-
-let rec contiene adjNodo cricca=
-  match cricca with
-      []->true
-    | x::rest->(List.mem x adjNodo && (contiene adjNodo rest));; 
-
-let rec succNodo graph n=
+let rec succNode graph n=
   match graph with
       []->[]
     | (x,s)::rest->
-      if(x==n)
-      then s
-      else (succNodo rest n);;
+        if(x==n)
+        then s
+        else (succNode rest n);;
 
-let rec succNodoConCricca succ cricca=
+let rec succNodeClique succ clique=
   match succ with
       []->[] 
-    | x::rest-> (x,cricca)::(succNodoConCricca rest cricca);;
+    | x::rest-> (x,clique)::(succNodeClique rest clique);;
 
-succNodoConCricca (succNodo grafo 1) [2;3];;
 
-let cricca graph k=
-  let rec aux coda graph =
-    match coda with
+
+let rec contains adjNode clique=
+  match clique with
+      []->true
+    | x::rest->(List.mem x adjNode && (contains adjNode rest));; 
+
+let clique graph k=
+  let rec aux queue graph =
+    match queue with
         []->[]
-      | (x,cricca)::rest-> if((contiene (succNodo graph x) cricca) && ((List.length (x::cricca))==k)) then cricca@[x] 
-      else if ((contiene (succNodo graph x) cricca) && ((List.length (x::cricca))<k)) then  (aux (rest@( succNodoConCricca (succNodo graph x) (cricca@[x]))) graph )
-      else (aux rest graph)
-  in aux (grafoAdjToGrafoAdjCricca graph) graph;;
+      | (x,clique)::rest-> 
+          if((contains (succNode graph x) clique) && ((List.length (x::clique))==k)) then clique@[x] 
+          else if ((contains (succNode graph x) clique) && ((List.length (x::clique))<k)) then  (aux (rest@( succNodeClique (succNode graph x) (clique@[x]))) graph )
+          else (aux rest graph)
+  in aux (queueForCliqueFromAdjGraph graph) graph;;
 
-cricca grafo 5;;
+
+let graph = [(1,[2;3;5]); (5,[2;3;4;1;7]) ;(7,[5;2;3;4]); (2,[4;1;5;3;7]); (3,[2;4;1;5;7]); (4,[2;3;5;7]) ];;
+
+clique graph 5;;
 
